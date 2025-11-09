1613780a1d69243724cf8fdd9974d045b7e6289a
PipeVerach <felipevera10@gmail.com>
2025-11-08 21:19:37 -0300

    new test

--- app/app.vue
+++ app/app.vue
@@ -7,8 +7,19 @@
 </template>
 
 <style>
+html, body {
+  background: linear-gradient(to bottom right, #f9fafb, #e5e7eb);
+  background-attachment: fixed;
+  background-size: cover;
+  width: 100%;
+  height: 100%;
+  margin: 0;
+  padding: 0;
+}
+
 body {
-  background: #F3D5E1;
+  min-height: 100vh;
+  min-height: 100dvh;
 }
 
 .page-enter-active,
