31937873db10b7d8beefa5a203942068945b286e
PipeVerach <felipevera10@gmail.com>
2025-11-08 21:21:42 -0300

    revert

--- app/app.vue
+++ app/app.vue
@@ -7,20 +7,6 @@
 </template>
 
 <style>
-html, body {
-  background: linear-gradient(to bottom right, #f9fafb, #e5e7eb);
-  background-attachment: fixed;
-  background-size: cover;
-  width: 100%;
-  height: 100%;
-  margin: 0;
-  padding: 0;
-}
-
-body {
-  min-height: 100vh;
-  min-height: 100dvh;
-}
 
 .page-enter-active,
 .page-leave-active {
